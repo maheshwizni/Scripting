6dc93022cdaf81a690f3e77fd7549a30ebbf88ad
amitthakkar <d0ntgetHyper>
2015-07-13 17:29:17 +0530

    ADDING home page.

--- ExcelUtility/main.js
+++ ExcelUtility/main.js
@@ -5,11 +5,25 @@
     var versionModule = ng.module('version', ['ngRoute']);
     versionModule.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/diff', {
+            page: 'diff',
             templateUrl: 'components/diff/_diff.html',
             controller: 'DiffController',
             controllerAs: 'diffController',
             controllerUrl: 'components/diff/diff.controller.js'
         });
-        $routeProvider.otherwise('/diff');
+        $routeProvider.when('/home', {
+            page: 'home',
+            templateUrl: 'components/home/_home.html',
+            controller: 'HomeController',
+            controllerAs: 'homeController',
+            controllerUrl: 'components/home/home.controller.js'
+        });
+        $routeProvider.otherwise('/home');
+    }]);
+    versionModule.controller('MasterController', ['$rootScope', function ($rootScope) {
+        var masterController = this;
+        $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+            masterController.page = current.$$route.page;
+        });
     }]);
 })(angular);
