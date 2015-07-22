17c4be2a4978aa2f33aa826e2c844968da5bdb10
amitthakkar <d0ntgetHyper>
2015-07-22 14:26:59 +0530

    Showing loading bar on version page.

--- ExcelUtility/components/version/version.controller.js
+++ ExcelUtility/components/version/version.controller.js
@@ -3,21 +3,28 @@
  */
 (function (ng) {
     var versionModule = ng.module('version');
-    versionModule.controller('VersionController', ['DiffService', function (DiffService) {
+    versionModule.controller('VersionController', ['DiffService', 'usSpinnerService', function (DiffService, usSpinnerService) {
         var versionController = this;
+        var loadingBarName = 'loadingVersionSpin';
+        usSpinnerService.spin(loadingBarName);
         DiffService.getSheetNameAndLatestVersion()
             .success(function (response) {
+                usSpinnerService.stop(loadingBarName);
                 versionController.sheetNameAndLatestVersion = response;
             }).error(function () {
+                usSpinnerService.stop(loadingBarName);
                 versionController.sheetNameAndLatestVersion = [];
             });
         versionController.showData = function() {
+            usSpinnerService.spin(loadingBarName);
+            versionController.data = undefined;
             DiffService.getSheetData(versionController.selectedSheet.sheetName, versionController.selectedVersion)
                 .success(function (response) {
+                    usSpinnerService.stop(loadingBarName);
                     versionController.data = JSON.parse(response.metaData);
-                    console.log(versionController.data);
                 })
                 .error(function () {
+                    usSpinnerService.stop(loadingBarName);
                     versionController.data = undefined;
                 });
         };
