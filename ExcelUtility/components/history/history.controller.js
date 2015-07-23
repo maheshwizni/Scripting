69b716f74b8c0891fb27f9c6f5ca870aaee290cb
amitthakkar <d0ntgetHyper>
2015-07-23 20:08:40 +0530

    Correcting history page

--- ExcelUtility/components/history/history.controller.js
+++ ExcelUtility/components/history/history.controller.js
@@ -3,11 +3,11 @@
  */
 (function (ng) {
     var versionModule = ng.module('version');
-    versionModule.controller('HistoryController', ['DiffService', 'usSpinnerService', function (DiffService, usSpinnerService) {
+    versionModule.controller('HistoryController', ['VersionService', 'usSpinnerService', function (VersionService, usSpinnerService) {
         var history = this;
         var loadingBarName = 'loadingVersionSpin';
         usSpinnerService.spin(loadingBarName);
-        DiffService.getSheetNameAndLatestVersion()
+        VersionService.getSheetNameAndLatestVersion()
             .success(function (response) {
                 usSpinnerService.stop(loadingBarName);
                 history.sheetNameAndLatestVersion = response;
@@ -18,7 +18,7 @@
         history.showData = function() {
             usSpinnerService.spin(loadingBarName);
             history.data = undefined;
-            DiffService.getSheetData(history.selectedSheet.sheetName, history.selectedVersion)
+            VersionService.getSheetData(history.selectedSheet.sheetName, history.selectedVersion)
                 .success(function (response) {
                     usSpinnerService.stop(loadingBarName);
                     history.data = JSON.parse(response.metaData);
